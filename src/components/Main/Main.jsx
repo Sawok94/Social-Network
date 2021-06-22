25d9ead0ff744c20937d79e9fde3c4f853ceaec5
Sawok <asswk1875@gmail.com>
2021-06-23 00:02:27 +0300

    Lazy|Suspense

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,28 @@
 import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import LoginContainer from '../Login/LoginContainer';
+import { withSuspense } from '../../hoc/withSuspense';
 import style from '../Main/Main.module.css';
-import Settings from '../Main/Settings/Settings';
-import FriendsContainer from './Friends/FriendsContainer';
-import MessagesContainer from './Messages/MessagesContainer';
-import ProfileContainer from './Profile/ProfileContainer';
+
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'));
+const MessagesContainer = React.lazy(() =>
+  import('./Messages/MessagesContainer')
+);
+const FriendsContainer = React.lazy(() => import('./Friends/FriendsContainer'));
+const Settings = React.lazy(() => import('../Main/Settings/Settings'));
+const LoginContainer = React.lazy(() => import('../Login/LoginContainer'));
 
 const Main = (props) => {
   return (
     <div className={style.main}>
-      <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
-      <Route path='/messages/:userId?' render={() => <MessagesContainer />} />
-      <Route path='/friends' render={() => <FriendsContainer />} />
-      <Route path='/settings' render={() => <Settings />} />
+      <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)} />
+      <Route
+        path='/messages/:userId?'
+        render={withSuspense(MessagesContainer)}
+      />
+      <Route path='/friends' render={withSuspense(FriendsContainer)} />
+      <Route path='/settings' render={withSuspense(Settings)} />
       {<Route path='' /> && <Redirect to={'/profile'} />}
-      <Route path='/login' render={() => <LoginContainer />} />
+      <Route path='/login' render={withSuspense(LoginContainer)} />
     </div>
   );
 };
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import profiles from './profiles-reducer';
 import messages from './messages-reducer';
 import users from './users-reducer';
@@ -16,6 +16,11 @@ const reducers = combineReducers({
   app,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
